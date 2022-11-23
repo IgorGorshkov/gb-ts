b11128948d8adcfd3ea17749c1985bf0fc5dca94
IgorGorshkov <91373076+IgorGorshkov@users.noreply.github.com>
2022-11-23 22:31:06 +0300

    lesson-3

--- src/user.ts
+++ src/user.ts
@@ -1,12 +1,47 @@
 import { renderBlock } from './lib.js';
 
+export class User {
+  username: string;
+  avatarUrl: string;
+  constructor(username: string, avatarUrl: string) {
+    this.username = username;
+    this.avatarUrl = avatarUrl;
+  }
+}
+
+export function getUserData(): unknown {
+  const userData: unknown = localStorage.getItem('user');
+  const data = typeof userData === 'string' ? JSON.parse(userData) : undefined;
+  if (typeof data === 'object' && 'username' in data && 'avatarUrl' in data) {
+    return new User(data.username, data.avatarUrl);
+  }
+
+  return 'Возможно вы не залогинены!';
+}
+
+export function getFavoritesAmount(): unknown {
+  const amountData: unknown = localStorage.getItem('favoritesAmount');
+  const amount =
+    typeof amountData === 'string' ? JSON.parse(amountData) : undefined;
+  if (!isNaN(Number(amount))) {
+    return Number(amount);
+  }
+
+  return false;
+}
+
 export function renderUserBlock(
   name: string,
   avatar: string,
-  favoriteItemsAmount: string | number
+  favoriteItemsAmount?: number
 ): void {
-  const favoritesCaption =
-    favoriteItemsAmount > 0 ? favoriteItemsAmount : 'ничего нет';
+  let favoritesCaption: string | number;
+  if (favoriteItemsAmount && favoriteItemsAmount > 0) {
+    favoritesCaption = favoriteItemsAmount;
+  } else {
+    favoritesCaption = 'ничего нет';
+  }
+
   const hasFavoriteItems = favoriteItemsAmount ? true : false;
 
   renderBlock(
