dcc381e44102578c66cb234d9edae5f9606d2d47
IgorGorshkov <91373076+IgorGorshkov@users.noreply.github.com>
2022-12-20 23:50:48 +0300

    lesson-4

--- public/scripts/search-form.js
+++ public/scripts/search-form.js
@@ -1,18 +1,70 @@
-import { renderBlock, getISODate, getLastDayOfMonth } from "./lib.js";
-export function renderSearchFormBlock(checkin = "", checkout = "") {
-    let minDate = new Date(), maxDate = new Date(), checkinDefaultDate = new Date(), checkoutDefaultDate = new Date();
+import { renderBlock, getISODate, getLastDayOfMonth, dateToUnixStamp, responseToJson, calculateDifferenceInDays } from './lib.js';
+import { renderSearchResultsBlock } from './search-results.js';
+import { FlatRentSdk } from './sdk/flat-rent-sdk.js';
+export const flatSDK = new FlatRentSdk();
+export function getFormData() {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const city = document.getElementById('city'), checkin = document.getElementById('check-in-date'), checkout = document.getElementById('check-out-date'), maxprice = document.getElementById('max-price'), coordinates = document.getElementById('coordinates');
+        const data = {
+            city: city.value,
+            checkin: new Date(checkin.value),
+            checkout: new Date(checkout.value),
+            maxprice: maxprice.value ? +maxprice.value : null,
+            coordinates: coordinates.value,
+        };
+        const sdkData = {
+            city: city.value,
+            checkInDate: new Date(checkin.value),
+            checkOutDate: new Date(checkout.value),
+            priceLimit: maxprice.value ? +maxprice.value : null
+        };
+        search(data).then((places) => {
+            places = places.map((place) => {
+                place.price = place.price * calculateDifferenceInDays(data.checkin, data.checkout);
+                return place;
+            });
+            flatSDK.search(sdkData).then((sdkpl) => {
+                const sdkPlaces = sdkpl.map((flat) => {
+                    return {
+                        id: flat.id,
+                        name: flat.title,
+                        image: flat.photos[0],
+                        description: flat.details,
+                        price: flat.totalPrice,
+                    };
+                });
+                places.push(...sdkPlaces);
+                renderSearchResultsBlock(places);
+            });
+        });
+    });
+}
+function search(data) {
+    let url = 'http://localhost:3030/places?' +
+        `checkInDate=${dateToUnixStamp(data.checkin)}&` +
+        `checkOutDate=${dateToUnixStamp(data.checkout)}&` +
+        `coordinates=${data.coordinates}`;
+    if (data.maxprice != null) {
+        url += `&maxPrice=${data.maxprice}`;
+    }
+    return responseToJson(fetch(url));
+}
+export function renderSearchFormBlock(checkin = '', checkout = '') {
+    const minDate = new Date(), maxDate = new Date(), checkinDefaultDate = new Date(), checkoutDefaultDate = new Date();
     maxDate.setMonth(maxDate.getMonth() + 1);
     maxDate.setDate(getLastDayOfMonth(maxDate.getFullYear(), maxDate.getMonth()));
     checkinDefaultDate.setDate(checkinDefaultDate.getDate() + 1);
     checkoutDefaultDate.setDate(checkoutDefaultDate.getDate() + 3);
-    renderBlock("search-form-block", `
-    <form>
+    renderBlock('search-form-block', `
+    <form id="form">
       <fieldset class="search-filedset">
         <div class="row">
           <div>
             <label for="city">Город</label>
-            <input id="city" type="text" disabled value="Севастополь" />
-            <input type="hidden" disabled value="59.9386,30.3141" />
+            <input id="city" name="city" type="text" disabled value="Санкт-Петербург" />
+            <input type="hidden" id="coordinates" name="coordinates" disabled value="59.9386,30.3141" />
           </div>
           <!--<div class="providers">
             <label><input type="checkbox" name="provider" value="homy" checked /> Homy</label>
--- src/search-results.ts
+++ src/search-results.ts
@@ -1,4 +1,6 @@
 import { renderBlock } from './lib.js';
+import { Place } from './search-form.js';
+import { renderUserInfo } from './user.js';
 
 export function renderSearchStubBlock() {
   renderBlock(
@@ -12,7 +14,51 @@ export function renderSearchStubBlock() {
   );
 }
 
-export function renderEmptyOrErrorSearchBlock(reasonMessage) {
+interface Favorite {
+  id: number;
+  name: string;
+  image: string;
+}
+
+export function toggleFavoriteItem(data: Place): void {
+  const itemsData: unknown = localStorage.getItem('favoriteItems');
+  const items =
+    typeof itemsData === 'string' ? JSON.parse(itemsData) : undefined;
+  const store = {};
+  if (inFavorite(data)) {
+    const values:Favorite[] = Object.values(items);
+    const filtered:Favorite[] = values.filter(function(item:Favorite) {
+      return item.id != data.id;
+    });
+    Object.assign(store, {
+      ...filtered
+    });
+  } else {
+    Object.assign(store, {
+      ...items,
+      [Object.keys(items).length]: { id: data.id, name: data.name, image: data.image },
+    });
+  }
+  if (typeof store === 'object') {
+    localStorage.setItem('favoritesAmount', String(Object.keys(store).length));
+    localStorage.setItem('favoriteItems', JSON.stringify(store));
+  }
+}
+
+export function inFavorite(data: Place): boolean {
+  const itemsData: unknown = localStorage.getItem('favoriteItems');
+  const items =
+    typeof itemsData === 'string' ? JSON.parse(itemsData) : undefined;
+  if (items && Object.keys(items).length > 0) {
+    const values:Favorite[] = Object.values(items);
+    return !!values.find(function (item: Favorite) {
+      return item.id == data.id;
+    });
+  }
+  return false;
+}
+
+export function renderEmptyOrErrorSearchBlock(reasonMessage: string) {
   renderBlock(
     'search-results-block',
     `
@@ -24,7 +70,33 @@ export function renderEmptyOrErrorSearchBlock(reasonMessage) {
   );
 }
 
-export function renderSearchResultsBlock() {
+export function renderSearchResultsBlock(data: Place[]): void {
+  const result = data.map((item: Place) => {
+    let classStr: string;
+    inFavorite(item) ? (classStr = 'active') : (classStr = '');
+    return `<li class="result">
+    <div class="result-container">
+      <div class="result-img-container">
+        <div class="favorites ${classStr}"></div>
+        <img class="result-img" src="${item.image}" alt="${item.name}">
+      </div>	
+      <div class="result-info">
+        <div class="result-info--header">
+          <p>${item.name}</p>
+          <p class="price">${item.price}&#8381;</p>
+        </div>
+        <div class="result-info--map"><i class="map-icon"></i> ${item.remoteness?item.remoteness+'км от вас':'расстояние не известно'}</div>
+        <div class="result-info--descr">${item.description}</div>
+        <div class="result-info--footer">
+          <div>
+            <button>Забронировать</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </li>`;
+  });
+
   renderBlock(
     'search-results-block',
     `
@@ -40,49 +112,15 @@ export function renderSearchResultsBlock() {
         </div>
     </div>
     <ul class="results-list">
-      <li class="result">
-        <div class="result-container">
-          <div class="result-img-container">
-            <div class="favorites active"></div>
-            <img class="result-img" src="./img/result-1.png" alt="">
-          </div>	
-          <div class="result-info">
-            <div class="result-info--header">
-              <p>YARD Residence Apart-hotel</p>
-              <p class="price">13000&#8381;</p>
-            </div>
-            <div class="result-info--map"><i class="map-icon"></i> 2.5км от вас</div>
-            <div class="result-info--descr">Комфортный апарт-отель в самом сердце Санкт-Петербрга. К услугам гостей номера с видом на город и бесплатный Wi-Fi.</div>
-            <div class="result-info--footer">
-              <div>
-                <button>Забронировать</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </li>
-      <li class="result">
-        <div class="result-container">
-          <div class="result-img-container">
-            <div class="favorites"></div>
-            <img class="result-img" src="./img/result-2.png" alt="">
-          </div>	
-          <div class="result-info">
-            <div class="result-info--header">
-              <p>Akyan St.Petersburg</p>
-              <p class="price">13000&#8381;</p>
-            </div>
-            <div class="result-info--map"><i class="map-icon"></i> 1.1км от вас</div>
-            <div class="result-info--descr">Отель Akyan St-Petersburg с бесплатным Wi-Fi на всей территории расположен в историческом здании Санкт-Петербурга.</div>
-            <div class="result-info--footer">
-              <div>
-                <button>Забронировать</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </li>
+      ${result.reduce((sum, current) => sum + current, '')}
     </ul>
     `
   );
+  document.querySelectorAll('.result .favorites').forEach((item, idx) => {
+    item.addEventListener('click', function () {
+      toggleFavoriteItem(data[idx]);
+      renderSearchResultsBlock(data);
+      renderUserInfo();
+    });
+  });
 }
